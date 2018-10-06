9224abfa1181ceecea2efa4e4152e68924aaf9d4
winerss <2857505905@qq.com>
2018-10-06 17:50:44 +0800

    2018106

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,5 +1,4 @@
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -64,54 +63,51 @@ Page({
       name: '宝蓝',
       key: '#4169E1'
     }],
-    imageList: [],
-    x: 0,
-    y: 0,
-    scale: 1,
-    add: '../../images/icon-add.png',
-    min: '../../images/icon-min.png',
-    rotate: '../../images/icon-rotate.png',
-    ok: '../../images/icon-ok.png',
-    remove: '../../images/icon-remove.png',
-    rotateVale: 0
+    ratio: 102 / 152,
+    originUrl: '',
+    cropperResult: ''
   },
-  tapadd() {
-    if (this.data.scale < 1.4) {
-      this.setData({
-        scale: this.data.scale + 0.1
-      })
-    }
-  },
-  tapmin() {
-    if (this.data.scale > 0.6) {
-      this.setData({
-        scale: this.data.scale - 0.1
-      })
-      console.log(1)
-    }
-  },
-  taprotate() {
-    this.setData({
-      rotateVale: this.data.rotateVale - 10
+  uploadTap() {
+    let _this = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        _this.setData({
+          originUrl: res.tempFilePaths[0],
+          cropperResult: ''
+        })
+      }
     })
   },
-  onChange(e) {
-    // console.log(e.detail)
+
+  onMyevent(e) {
+    var cropData = e.detail;
+    console.log(e)
+    const ctx = wx.createCanvasContext('canvas0', this);
+    ctx.drawImage(this.data.img, 0, 0, 320, 360)
+    ctx.draw()
+    // 缩放偏移值
+    let x = (cropData.width - cropData.width * cropData.scale) / 2;
+    let y = (cropData.height - cropData.height * cropData.scale) / 2;
+
+    //画布中点坐标转移到图片中心
+    let movex = (160 - (cropData.scale * cropData.width) / 2+cropData.offsetX + x);
+    let movey = (cropData.offsetY + y);
+    ctx.translate(movex, movey);
+    ctx.rotate(cropData.rotate * Math.PI / 180);
+    ctx.translate(-movex, -movey);
+
+    ctx.drawImage(cropData.url, 160 - (cropData.scale * cropData.width) / 2 + cropData.offsetX, cropData.offsetY, cropData.width * cropData.scale, cropData.height * cropData.scale);
+    // ctx.rotate(60 * Math.PI / 180);
+    // ctx.drawImage(this.data.originUrl, 160 - (p.scale * p.width) / 2 + p.offsetX,p.offsetY, p.scale * (p.width), p.scale * (p.height))
+    ctx.draw(true)
   },
-  onScale(e) {
-    // console.log(e.detail)
-  },
-  chooseImg: function () {
-    var that = this;
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        that.setData({
-          imageList: res.tempFilePaths
-        })
-      },
+  getCropperImg(e) {
+    this.setData({
+      originUrl: '',
+      cropperResult: e.detail.url
     })
   },
   previewImage: function (e) {
